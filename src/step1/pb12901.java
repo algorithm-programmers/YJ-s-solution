083c3392ce3ee83c63b4284a2cb13e8a736b6e73
yeojinkim94@gmail.com <yeoinkim94@gmail.com>
2020-01-02 17:32:00 +0900

    연습문제 > 2016년

---
+++ src/step1/pb12901.java
@@ -0,0 +1,26 @@
+package step1;
+
+public class pb12901 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 24;
+		
+		System.out.println(solution(a,b));
+	}
+	
+	// 2016년 1월 1일 : 금 (week[5])
+	public static String solution(int a, int b) {
+		String[] week = {"SUN", "MON","TUE","WED","THU","FRI","SAT"};
+		int[] month = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+		int day_sum = b;
+		
+		for(int i=1; i<a; i++) {
+			day_sum += month[i];
+		}
+		
+		int remain = (day_sum + 4) % 7;
+		
+		return week[remain];
+	}
+}
