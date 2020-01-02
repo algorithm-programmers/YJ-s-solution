840a3229f2355c21ab8134280c4c58b65c557c0e
yeojinkim94@gmail.com <yeoinkim94@gmail.com>
2020-01-02 16:35:04 +0900

    코딩테스트 연습 > 해시 > 완주하지 못한 선수(42576)

---
+++ src/step1/pb42576.java
@@ -0,0 +1,29 @@
+package step1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class pb42576 {
+	public static void main(String[] args) {
+		String str1[] = {"marina", "josipa", "nikola", "vinko", "filipa"};
+		String str2[] = {"josipa", "filipa", "marina", "nikola"};
+		
+		System.out.println(solution(str1, str2));
+	}
+	// participant -1 = completion
+	public static String solution(String[] participant, String[] completion) {
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		
+		String answer = participant[participant.length - 1];
+		
+		for(int i=0; i<completion.length; i++) {
+			if(!participant[i].equals(completion[i])) {
+				return participant[i];
+			}
+		}
+		
+		return answer;
+    }
+}
