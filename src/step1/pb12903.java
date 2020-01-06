20b26868d8beb8dda462d543c1a9e9c65af4ba52
yeojinkim94@gmail.com <yeoinkim94@gmail.com>
2020-01-06 17:20:14 +0900

    가운데 글자 가져오기, 같은 숫자는 싫어

---
+++ src/step1/pb12903.java
@@ -0,0 +1,23 @@
+package step1;
+
+// 연습문제 > 가운데 글자 가져오기
+public class pb12903 {
+	public static void main(String args[]) {
+		//String s = "abcde";
+		String s = "qw";
+		
+		System.out.println(solution(s));
+	}
+	
+	public static String solution(String s) {
+		String answer = "";
+		
+		if(s.length() % 2 == 0) { // 짝수
+			answer += Character.toString(s.charAt(s.length()/2 - 1)) + Character.toString(s.charAt(s.length()/2));
+		} else {
+			answer += Character.toString(s.charAt(s.length()/2));
+		}
+		
+		return answer;
+	}
+}
---
+++ src/step1/pb12906.java
@@ -0,0 +1,39 @@
+package step1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// 연습문제 > 같은 숫자는 싫어
+public class pb12906 {
+	
+	public static void main(String args[]) {
+		int[] arr = {1,1,3,3,0,1,1};
+		//int[] arr = {4,4,4,3,3};
+		
+		solution(arr);
+	}
+	
+	private static int[] solution(int[] arr) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(arr[0]);
+		
+		for(int i=1; i<arr.length; i++) {
+			int temp = arr[i-1];
+			
+			if(temp != arr[i]) {
+				list.add(arr[i]);
+				
+			}
+		}
+		
+		int[] answer = new int[list.size()];
+		int idx = 0;
+		
+		for(int a : list) {
+			answer[idx] = a;
+			idx ++;
+		}
+		return answer;
+	}
+
+}
