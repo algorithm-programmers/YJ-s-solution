81074453bb59d0c5df92693600af1d8e0dc46ae5
yeoj1n <yeojinkim94@gmail.com>
2020-01-21 23:41:57 +0900

    60057 문자열 압축

--- src/programmers/step1/pb60057.java
+++ src/programmers/step1/pb60057.java
@@ -1,16 +1,68 @@
 package programmers.step1;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// [2020카카오공채] 문자열 압축
 public class pb60057 {
 	public static void main(String args[]) {
-		String str = "";
+		//String str = "aabbaccc";
+		//	String str = "ababcdcdababcdcd";
+//		String str = "abcabcdede";
+		String str = "abcabcabcabcdededededede";
+		//String str = "xababcdcdababcdcd";
 		solution(str);
+	
 	}
 	
 	public static int solution(String str) {
 		int len = str.length();
-		int answer = len;
-		int half = len/2;
-		return answer;
+		
+ 		for(int i=1; i<=str.length()/2; i++) {
+			Queue<String> que = new LinkedList<String>();
+		
+			int j = 1;
+			while(i * j <= str.length()) {
+				que.offer(str.substring((j-1)*i, (j-1)*i + i));
+				j ++;
+			}
+			
+			if(i*j != str.length()) {
+				que.offer(str.substring(i*(j-1)));
+			}
+			
+			int count = 1;
+			StringBuffer sb = new StringBuffer();
+			
+			while(!que.isEmpty()) {
+				String poll = que.poll();
+				String peek = que.peek();
+				
+				if(poll.equals(peek)) { // 앞, 뒤 같을 때
+					count ++;
+				} else { // 앞, 뒤 다를 때
+					if(count != 1) {
+						sb.append(count + poll);
+						count = 1;
+					} else {
+						sb.append(poll);
+					}
+				}
+				if(que.size() == 1 && count != 1) {
+					sb.append(count + peek);
+					que.poll();
+				}
+			}
+			
+			System.out.println(sb.toString());
+			
+			if(sb.length() < len) {
+				len = sb.length();
+			}
 		}
+ 		System.out.println("가장 짧은 길이: " + len);
+		return len;
+	}
 
 }
