50008e4a56b06da4de15edbdea633f1d888783e8
yeoj1n <yeojinkim94@gmail.com>
2020-01-29 00:44:44 +0900

    programmers level1 solve

--- src/programmers/step1/pb12940.java
+++ src/programmers/step1/pb12940.java
@@ -7,26 +7,17 @@ public class pb12940 {
 	}
 	
 	public int[] solution(int n, int m) {
-	      int[] answer = new int[2];
-	      
-	      if(m % n == 0) {
-	    	  answer[0] = n;
-	    	  answer[1] = m;
-	    	  return answer;
-	      }
-	      
-	      int max = Math.max(n, m);
-	      int min = Math.min(n, m);
-	      
-	      int g = 1;
-	      for(int i=2; i<n; i++) {//n으로는 나누어지지 않고 1은 모든 수가 나누어짐. 2부터 시
-	    	  if(max % i == 0) {
-	    		  g = i;
-	    	  }
-	      }
-	      answer[0] = g;
-	      answer[1] = min * max / g; 
-	      
-	      return answer;
+		int[] answer = new int[2];
+		int ori_n = n;
+		int ori_m = m;
+		while(m > 0) {
+			int tmp = m;
+			m = n % m;
+			n = tmp;
+		}
+		answer[0] = n;
+		answer[1] = ori_m * ori_n / n; 
+		
+		return answer;
 	  }
 }
